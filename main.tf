0b33dde442af449a95c50ea712a1ae8be3bac07e
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-19 19:02:01 +0530

    recheck the module

--- main.tf
+++ main.tf
@@ -8,25 +8,26 @@ resource "random_string" "cosmos_server_postfix" {
 
 resource "azurerm_cosmosdb_account" "db" {
 
-  name                    = "${var.name}-${random_string.cosmos_server_postfix.id}-account"
-  location                = var.location
-  resource_group_name     = var.resource_group_name 
-  offer_type              = var.offer_type
-  kind                    = var.kind
+  name                      = "${var.name}-${random_string.cosmos_server_postfix.id}-account"
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  offer_type                = var.offer_type
+  kind                      = var.kind
   enable_automatic_failover = true
+  tags                      = var.tags
 
   capabilities {
-      name = var.capabilities
+    name = var.capabilities
   }
 
   consistency_policy {
-      consistency_level       = var.consistency_level
-      max_interval_in_seconds = var.max_interval_in_seconds
-      max_staleness_prefix    = var.max_staleness_prefix
+    consistency_level       = var.consistency_level
+    max_interval_in_seconds = var.max_interval_in_seconds
+    max_staleness_prefix    = var.max_staleness_prefix
   }
   geo_location {
-      location          = var.location
-      failover_priority = 0
+    location          = var.location
+    failover_priority = 0
   }
 
   lifecycle {
--- variables.tf
+++ variables.tf
@@ -1,56 +1,56 @@
 // required variables
-variable resource_group_name {
+variable "resource_group_name" {
   type        = string
   description = "name of the resource group where all the resources will be created"
 }
 
-variable name {
+variable "name" {
   type        = string
   description = "name of the resource group where all the resources will be created"
 }
 
-variable offer_type {
+variable "offer_type" {
   type        = string
   description = "name of the resource group where all the resources will be created"
 }
 
-variable mongo_server_version {
+variable "consistency_level" {
   type        = string
   description = "name of the resource group where all the resources will be created"
 }
 
-variable consistency_level {
-  type        = string
-  description = "name of the resource group where all the resources will be created"
-}
-
-variable mongodb_throughput {
+variable "mongodb_throughput" {
   type        = number
   description = "name of the resource group where all the resources will be created"
+  default = 400
 }
 
-variable max_interval_in_seconds {
+variable "max_interval_in_seconds" {
   type        = number
   description = "name of the resource group where all the resources will be created"
+  default = 5
+
 }
 
-variable max_staleness_prefix {
+variable "max_staleness_prefix" {
   type        = number
   description = "name of the resource group where all the resources will be created"
+  default = 100
+
 }
 
 variable "kind" {
-  type   = string
+  type        = string
   description = "Specifies the Kind of CosmosDB to create - possible values are GlobalDocumentDB and MongoDB"
-  default = "MongoDB"
+  default     = "MongoDB"
 }
 
 variable "capabilities" {
-  type = string
+  type        = string
   description = "Configures the capabilities to enable for this Cosmos DB account"
 }
 
 variable "location" {
-  type = string
+  type        = string
   description = "Specifies the supported Azure location where the resource exists˝"
 }
